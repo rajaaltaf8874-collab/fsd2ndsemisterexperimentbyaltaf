f7c9679e0c948c94a49b743e12d609eb3e402f7d
Altafraja <rajaaltaf8874@gmail.com>
2026-06-19 13:20:14 +0530

    Add files via upload

---
+++ experiment-6/student/src/components/Student.jsx
@@ -0,0 +1,17 @@
+function Student(props) {
+  return (
+    <div className="card">
+      <h2>{props.name}</h2>
+
+      <p>
+        <strong>Course:</strong> {props.course}
+      </p>
+
+      <p>
+        <strong>Marks:</strong> {props.marks}
+      </p>
+    </div>
+  );
+}
+
+export default Student;
